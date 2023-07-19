8006b3c5b771f4d43f9e97876ca4fc1a76bc7374
Ermiyas B <124329320+Ermi-B@users.noreply.github.com>
2023-07-18 19:47:54 -0500

    added login functionality on back end

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -42,4 +42,63 @@ router.post("/", async (req, res) => {
 });
 
 
+router.delete('/:id',async(req,res)=>{
+   
+try{
+   await User.destroy({
+     where:{
+       id:req.params.id
+     }
+   }).then((rows)=>{
+     res.status(200).json({message:rows+ ' rows(s) affected'})
+   })
+  }catch(err){
+   res.status(500).json({message:err})
+  }
+});
+
+
+router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { name: req.body.username } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect username or password, please try again' });
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect username or password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.logged_in = false;
+      req.session.destroy(() => {
+        res.status(204).end();
+      }); 
+    } else {
+      res.status(404).end();
+    }
+  });
+
 module.exports = router;
